e369b6166b80145f23e4321c5eb9c03efe4be3be
liviarez <mrezende.livia@gmail.com>
2023-06-09 14:41:20 -0300

    Feat: Test App Component. Req 1

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,5 @@
-test('', () => {});
+describe('Teste o componente About.js', () => {
+  it('Teste se a página contém as informações sobre a Pokédex', () => {
+    expect(numbers([1, 2, 3, 4, 5])).toEqual(true);
+  });
+});
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { screen/* , render */ } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa o componente <App.js />', () => {
+  it('Teste se a aplicação é redirecionada para a página de Home', () => {
+    // cria um historico de navegacao
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'Home' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    expect(history.location.pathname).toBe('/');
+  });
+
+  it('Teste se a aplicação é redirecionada para a página de About', () => {
+    const { history } = renderWithRouter(<App />);
+    const homeLink = screen.getByRole('link', { name: 'About' });
+    expect(homeLink).toBeInTheDocument();
+
+    userEvent.click(homeLink);
+
+    expect(history.location.pathname).toBe('/about');
+  });
+});
+
+// Referencia do course da Trybe. Dia 03. Testes no Router.
